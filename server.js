b13bd527f5873f8f01392ed640b553c05ea78046
Elena Carey <theelenacarey@gmail.com>
2022-07-15 17:27:47 -0500

    Wrote git post for notes.

--- server.js
+++ server.js
@@ -1,14 +1,75 @@
 const express = require('express');
+const fs = require('fs');
 
-const { notes } = require('./data/notes.json');
+const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+const { notes } = require('./data/notes.json');
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+//connect public
+app.use(express.static('public'));
+
+
+
+function createNewNote(body, notesArr) {
+    const note = body;
+    notesArr.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, './data/notes.json'),
+        JSON.stringify({notes: notesArr}, null, 2)
+      );
+    return note;
+}
+
+//get notes and show results
 app.get('/api/notes', (req, res) => {
     res.json(notes);
 });
 
+app.get('/api/notes/:id', (req, res) => {
+    const result = findById(req.params.id, notes);
+    if (result) {
+        res.json(result);
+        console.log(result);
+        console.info(`${req.method} request received for notes`)
+    } else {
+        res.send(404);
+    }
+});
+
+// app.get('/api/notes', (req, res) => {
+    
+// })
+
+app.post('/api/notes', (req, res) => {
+    // req.body is where our incoming content will be
+    console.log(req.body);
+    req.body.id = notes.length.toString();
+
+    const note = createNewNote(req.body, notes)
+    res.json(note);
+});
+
+
+
+//connecting html pages
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index'));
+});
+//get html notes
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes'));
+});
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index'));
+});
 app.listen(PORT, () => {
     console.log(`API server is now on port ${PORT}`);
 });
+
